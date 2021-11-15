2bb0b68cde19e22caa2774df5b20209e699e0a69
Arunsh93 <Arunhosur93@gmail.com>
2021-11-15 14:17:05 +0530

    Add Employee Payroll data JS

---
+++ EmployeePayrollFormApp/JavaScript/EmployeePayrollData.js
@@ -0,0 +1,68 @@
+class EmployeePayrollJsData{
+    get id(){
+        return this._id;
+    }
+
+    set id(id){
+        this._id = id;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    set name(name){
+        this._name = name;
+    }
+
+    get profilePic(){
+        return this._profilePic;
+    }
+
+    set profilePic(profilePic){
+        this._profilePic = profilePic;
+    }
+
+    get gender(){
+        return this._gender;
+    }
+
+    set gender(gender){
+        this._gender = gender;
+    }
+
+    get department(){
+        return this._department;
+    }
+
+    set department(department){
+        this._department = department;
+    }
+
+    get salary(){
+        return this._note;
+    }
+
+    set salary(salary){
+        this._salary = salary;
+    }
+
+    get note(){
+        return this._note;
+    }
+
+    set note(note){
+        this._note = note;
+    }
+
+    get startDate(){
+        return this._startDate;
+    }
+
+    toString(){
+        const options = { year: 'numeric', month: 'long', day: 'numeric'};
+        const empDate = !this.startDate ? "undefined" : this.startDate.toLocaleDateString("en-us", options);
+        return "\nID = " + this.id + ", \Name = " + this.name + ", \nGender = " + this.gender + ", \nProfile Pic = " + this.profilePic + 
+                    ", \nDepartment = " + this.department + ", \nSalary = " + this.salary + ", \nStart Date = " + empDate + ", \nNote = " + this.note; 
+    }
+}
